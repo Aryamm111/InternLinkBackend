928f48ca5741af9842c65cb29f8b3d41f25be7a9
Arwaahmed100 <arwaa2490@gmail.com>
2025-04-20 19:17:39 +0300

    Update HRManager.java

--- src/main/java/com/internlink/internlink/model/HRManager.java
+++ src/main/java/com/internlink/internlink/model/HRManager.java
@@ -1,15 +1,25 @@
 package com.internlink.internlink.model;
-
+//New HR Model 
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import java.util.List;
 
 @Document(collection = "HRmanager")
 public class HRManager extends User {
 
     @Indexed(unique = true)
     private String HRmanagerId;
-    public String companyName;
+    private String companyName;
 
+ @DBRef
+    private List<Application> applications; // 1 to 1..* relationship with Application
+
+ @DBRef
+    private List<Internship> internshipOpportunities; // 1 to 1..* relationship with InternshipOpportunity
+
+
+    
     public String getHRmanagerId() {
         return HRmanagerId;
     }
@@ -26,4 +36,20 @@ public class HRManager extends User {
         this.companyName = companyName;
     }
 
-}
+     public List<Application> getApplications() {
+        return applications;
+    }
+
+    public void setApplications(List<Application> applications) {
+        this.applications = applications;
+    }
+
+    public List<Internship> getInternshipOpportunities() {
+        return internshipOpportunities;
+    }
+
+    public void setInternshipOpportunities(List<Internship> internshipOpportunities) {
+        this.internshipOpportunities = internshipOpportunities;
+    }
+
+} // END HR model
