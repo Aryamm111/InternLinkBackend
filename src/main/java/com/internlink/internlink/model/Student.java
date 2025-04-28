f3c1a60b74d6d430afbecd60531171c1723b6810
aryam <aryamm0002@gmail.com>
2025-04-28 16:02:31 +0300

    update

--- src/main/java/com/internlink/internlink/model/Student.java
+++ src/main/java/com/internlink/internlink/model/Student.java
@@ -1,29 +1,48 @@
 package com.internlink.internlink.model;
 
-import org.springframework.data.mongodb.core.index.Indexed;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "students")
 public class Student extends User {
 
-    @Indexed(unique = true)
+    @Id
     private String studentId;
     private String major;
-    private String university;
     private String facultySupervisorId;
     private String companySupervisorId;
+
+    private String location;
+    private List<String> skills;
+    private List<Float> embedding;
+    private int earnedHours;
+    private int remainingHours;
+    private double GPA;
 
     public Student() {
         super();
     }
 
-    public Student(String email, String password, String name, String major, String university) {
+    public Student(String email, String password, String name, String major, String facultySupervisorId,
+            String companySupervisorId, String location, List<String> skills, List<Float> embedding,
+            int earnedHours, int remainingHours, double GPA) {
         super(email, password, name);
         this.major = major;
-        this.university = university;
+        this.facultySupervisorId = facultySupervisorId;
+        this.companySupervisorId = companySupervisorId;
+        this.location = location;
+        this.skills = skills;
+        this.embedding = embedding;
+        this.earnedHours = earnedHours;
+        this.remainingHours = remainingHours;
+        this.GPA = GPA;
     }
 
-    public String getStudentId() {
+    // Getters and Setters
+    @Override
+    public String getId() {
         return studentId;
     }
 
@@ -39,16 +58,8 @@ public class Student extends User {
         this.major = major;
     }
 
-    public String getUniversity() {
-        return university;
-    }
-
-    public void setUniversity(String university) {
-        this.university = university;
-    }
-
     public String getFacultySupervisorId() {
-        return this.facultySupervisorId;
+        return facultySupervisorId;
     }
 
     public void setFacultySupervisorId(String facultySupervisorId) {
@@ -56,11 +67,58 @@ public class Student extends User {
     }
 
     public String getCompanySupervisorId() {
-        return this.companySupervisorId;
+        return companySupervisorId;
     }
 
     public void setCompanySupervisorId(String companySupervisorId) {
         this.companySupervisorId = companySupervisorId;
     }
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<String> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<String> skills) {
+        this.skills = skills;
+    }
+
+    public List<Float> getEmbedding() {
+        return embedding;
+    }
+
+    public void setEmbedding(List<Float> embedding) {
+        this.embedding = embedding;
+    }
+
+    public int getEarnedHours() {
+        return earnedHours;
+    }
+
+    public void setEarnedHours(int earnedHours) {
+        this.earnedHours = earnedHours;
+    }
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(double GPA) {
+        this.GPA = GPA;
+    }
+
+    public int getRemainingHours() {
+        return remainingHours;
+    }
+
+    public void setRemainingHours(int remainingHours) {
+        this.remainingHours = remainingHours;
+    }
 }
